2ccf9c4d5ab0bcd4fb49d1e03ce3e4047c23703f
saha <saha@sahas-MacBook-Air.local>
2024-11-01 14:24:56 +0530

    pipeline changes

--- target/compiled/DBT_CI_CD_DEMO/models/fct/fct_reviews.sql
+++
@@ -1,7 +0,0 @@
- 
-WITH src_reviews AS (
-    SELECT * FROM airbnb.raw.src_reviews
-)
-SELECT * FROM src_reviews WHERE review_text is not null 
-
- AND review_date > ( select max(review_date) from airbnb.raw.fct_reviews ) 
--- target/compiled/dbt_learn/models/fct/fct_reviews.sql
+++
@@ -1,5 +0,0 @@
- 
-WITH src_reviews AS (
-    SELECT * FROM airbnb.raw.src_reviews
-)
-SELECT * FROM src_reviews WHERE review_text is not null 
